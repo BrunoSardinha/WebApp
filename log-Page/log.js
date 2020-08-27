d047a76de5c65893de42ba61631165e65a58522f
BrunoSardinha <bruno@sapoznik.com.br>
2020-08-27 18:22:42 -0300

    save user info

--- log-Page/log.js
+++ log-Page/log.js
@@ -1,3 +1,5 @@
 function onClick(){
-    console.log("zUp")
+    const username = document.getElementById("Username").value;
+    const password = document.getElementById("tururu").value;
+    console.log(username,password)
 }
